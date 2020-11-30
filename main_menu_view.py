f516ca14fe07f492217f8002bfda2b614d507849
Steven Yu <sky3947@rit.edu>
2020-11-30 01:04:36 -0500

    Added the MINESWEEPER banner

--- main_menu_view.py
+++ main_menu_view.py
@@ -5,7 +5,7 @@ start, or delete a Minesweeper game.
 
 from view import View
 from utility import Action, Point
-from uielement import TextBox
+from uielement import TextBox, LongTextBox
 
 class MainMenuView(View):
     """
@@ -14,9 +14,22 @@ class MainMenuView(View):
     """
     def __init__(self, controller):
         super().__init__(controller)
+        grap = self.graphics
 
-        self.test_text = TextBox(Point(0, 10), "test textbox")
-        self.test_text.set_color(self.graphics.BRIGHT)
+        banner = [
+			r"  __ __ _ __  _ ___  __  _   _ ___ ___ ___ ___ ___  ",
+			r" |  V  | |  \| | __/' _/| | | | __| __| _,\ __| _ \ ",
+			r" | \_/ | | | ' | _|`._`.| 'V' | _|| _|| v_/ _|| v / ",
+			r" !_! !_!_!_!\__!___!___/!_/ \_!___!___!_! !___!_!_\ "
+		]
+        start_point, _ = grap.center_just(4, banner[0])
+        title = LongTextBox(start_point, banner)
+        title.set_color(grap.BRIGHT)
+        self.uielements.append(title)
+
+        test_text = TextBox(Point(0, 10), "test textbox")
+        test_text.set_color(grap.BRIGHT)
+        self.uielements.append(test_text)
 
     def parse(self, inp):
         """
@@ -30,12 +43,3 @@ class MainMenuView(View):
             Action: The action to perform.
         """
         return Action("", [])
-
-    def draw(self):
-        """
-        Draws the main menu.
-        """
-        graph = self.graphics
-        graph.clear()
-
-        self.test_text.draw(graph)
