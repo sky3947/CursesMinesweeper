2a7e1b1a547a8c66e3cd7b968616e754109ec098
Steven Yu <sky3947@rit.edu>
2020-12-01 11:57:45 -0500

    Restructured and added some controls.

--- main_menu_view.py
+++ main_menu_view.py
@@ -4,7 +4,7 @@ start, or delete a Minesweeper game.
 """
 
 from view import View
-from utility import Action, Point
+from utility import Action, Point, Direction
 from uielement import TextBox, LongTextBox, Button
 
 class MainMenuView(View):
@@ -14,31 +14,118 @@ class MainMenuView(View):
     """
     def __init__(self, controller):
         super().__init__(controller)
-        grap = self.graphics
 
         # The hovered input when entering this View.
         self.first_inp = "m"
 
-        # Color options.
-        color = grap.BRIGHT
-        hovered_color = grap.HIGHLIGHT
-        disabled_color = grap.DIM
+        # Make the MINESWEEPER logo.
+        self.make_banner()
 
-        # MINESWEEPER logo.
-        banner = [
-			R"  __ __ _ __  _ ___  __  _   _ ___ ___ ___ ___ ___  ",
-			R" |  V  | |  \| | __/' _/| | | | __| __| _,\ __| _ \ ",
-			R" | \_/ | | | ' | _|`._`.| 'V' | _|| _|| v_/ _|| v / ",
-			R" !_! !_!_!_!\__!___!___/!_/ \_!___!___!_! !___!_!_\ "
-		]
-        start_point, _ = grap.center_just(4, banner[0])
-        title = LongTextBox(start_point, banner)
-        title.set_color(color)
-        self.uielements.append(title)
+        # Make Buttons
+        self.make_buttons()
+
+        # Make the information box. This explains each Button.
+        self.make_info_box()
+
+        # Make controls bar.
+        self.make_controls_bar()
+
+        # Map of input to functions.
+        self.controls = {
+            "q": lambda: Action("quit", []),
+            "w": lambda: self.move_cursor(Direction.U),
+            "a": lambda: self.move_cursor(Direction.L),
+            "s": lambda: self.move_cursor(Direction.D),
+            "d": lambda: self.move_cursor(Direction.R)
+        }
+
+    def move_cursor(self, direction):
+        movement = 1
+        last_input = ""
+        if direction is Direction.U:
+            movement = -1
+            last_input = "w"
+        elif direction is Direction.L:
+            movement = -1
+            last_input = "a"
+        elif direction is Direction.D:
+            last_input = "s"
+        elif direction is Direction.R:
+            last_input = "d"
+
+        has_enabled_buttons = any(but.is_enabled() for but in self.buttons)
+        if has_enabled_buttons:
+            # Find the next enabled Button.
+            cur_but_ind = self.buttons.index(self.selected)
+            next_but_ind = (cur_but_ind + movement) % len(self.buttons)
+            while not self.buttons[next_but_ind].is_enabled:
+                next_but_ind = (next_but_ind + movement) % len(self.buttons)
+            next_enabled_button = self.buttons[next_but_ind]
+
+            self.selected.set_hovered(False)
+            next_enabled_button.set_hovered(True)
+            self.selected = next_enabled_button
+        else:
+            return Action("", [])
+
+        return Action("set last input", [last_input])
+
+    def make_controls_bar(self):
+        """
+        Creates the controls bar to display controls.
+        """
+        # Color options.
+        color = self.graphics.BRIGHT
+
+        text = [
+            "_"*self.graphics.LENGTH,
+            " wasd: Move | m: Select | q: Quit"
+        ]
+        controls = LongTextBox(Point(0, self.graphics.HEIGHT-3), text)
+        controls.set_color(color)
+        self.uielements.append(controls)
+
+    def update_information_box_text(self):
+        """
+        Updates the information box.
+        """
+        message = ""
+        if self.selected is self.buttons[0]:
+            message = "Continue a saved game."
+        elif self.selected is self.buttons[1]:
+            message = "Start a new game."
+        elif self.selected is self.buttons[2]:
+            message = "Delete the saved game."
+        self.info_box.set_text(message)
+
+    def make_info_box(self):
+        """
+        Creates the info box to explain each Button.
+        """
+        # Color options.
+        color = self.graphics.BRIGHT
+
+        self.info_box = TextBox(Point(1, 22))
+        self.info_box.set_color(color)
+        self.uielements.append(self.info_box)
+        self.update_information_box_text()
+
+        info_box_bar = TextBox(Point(0, 21), "_"*self.graphics.LENGTH)
+        info_box_bar.set_color(color)
+        self.uielements.append(info_box_bar)
+
+    def make_buttons(self):
+        """
+        Initializes and adds Buttons.
+        """
+        # Color options.
+        color = self.graphics.BRIGHT
+        hovered_color = self.graphics.HIGHLIGHT
+        disabled_color = self.graphics.DIM
 
         # Delete save Button.
         text = "Delete Save"
-        centered_point, _ = grap.center_just(15, text)
+        centered_point, _ = self.graphics.center_just(15, text)
         delete_save_button = Button(centered_point, text)
         delete_save_button.set_color(color)
         delete_save_button.set_hovered_color(hovered_color)
@@ -62,47 +149,32 @@ class MainMenuView(View):
         # Keep track of Buttons.
         self.buttons = [continue_button, new_game_button, delete_save_button]
 
-        self.selected = 0
+        self.selected = continue_button
         if self.controller.has_saved_game():
-            self.selected = 0
             continue_button.set_hovered(True)
         else:
-            self.selected = 1
+            self.selected = new_game_button
             new_game_button.set_hovered(True)
             continue_button.set_enabled(False)
             delete_save_button.set_enabled(False)
 
-        # Make information box. (Explains what the hovered Button does.)
-        self.info_box = TextBox(Point(1, 22))
-        self.info_box.set_color(color)
-        self.uielements.append(self.info_box)
-        self.update_information_box_text()
-
-        info_box_bar = TextBox(Point(0, 21), "_"*grap.LENGTH)
-        info_box_bar.set_color(color)
-        self.uielements.append(info_box_bar)
-
-        # Make controls bar.
-        text = [
-            "_"*grap.LENGTH,
-            " wasd: Move | m: Select | q: Quit"
-        ]
-        controls = LongTextBox(Point(0, grap.HEIGHT-3), text)
-        controls.set_color(color)
-        self.uielements.append(controls)
-
-    def update_information_box_text(self):
+    def make_banner(self):
         """
-        Updates the information box.
+        Creates the MINESWEEPER banner.
         """
-        message = ""
-        if self.selected == 0:
-            message = "Continue a saved game."
-        elif self.selected == 1:
-            message = "Start a new game."
-        elif self.selected == 2:
-            message = "Delete the saved game."
-        self.info_box.set_text(message)
+        # Color options.
+        color = self.graphics.BRIGHT
+
+        banner = [
+			R"  __ __ _ __  _ ___  __  _   _ ___ ___ ___ ___ ___  ",
+			R" |  V  | |  \| | __/' _/| | | | __| __| _,\ __| _ \ ",
+			R" | \_/ | | | ' | _|`._`.| 'V' | _|| _|| v_/ _|| v / ",
+			R" !_! !_!_!_!\__!___!___/!_/ \_!___!___!_! !___!_!_\ "
+		]
+        start_point, _ = self.graphics.center_just(4, banner[0])
+        title = LongTextBox(start_point, banner)
+        title.set_color(color)
+        self.uielements.append(title)
 
     def parse(self, inp):
         """
@@ -115,4 +187,6 @@ class MainMenuView(View):
         Returns:
             Action: The action to perform.
         """
-        return Action("", [])
+        inp = inp.lower()
+        fun = self.controls.get(inp, lambda: Action("", []))
+        return fun()
